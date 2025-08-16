5d9b81822921f8603e89e99bb08dbd7b835109a0
GalloJunior <gallojunior@gmail.com>
2025-08-15 22:35:20 -0300

    Finalizada a Home e criada a view postagem

--- BlogGallo/Models/Categoria.cs
+++ BlogGallo/Models/Categoria.cs
@@ -9,4 +9,15 @@ public class Categoria
     [Required]
     [StringLength(30)]
     public string Nome { get; set; }
+
+    [StringLength(7)]
+    public string Cor { get; set; } = "#000000";
+
+    public Categoria() { }
+
+    public Categoria(int id, string nome)
+    {
+        Id = id;
+        Nome = nome;
+    }
 }
